12236689ea8ab593af18c4ad50db3c55da64fa35
taheito26-sys <taheito26@gmail.com>
2026-04-02 02:51:59 +0300

    y

--- src/App.tsx
+++ src/App.tsx
@@ -182,7 +182,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-         <BrowserRouter>
+         <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <NativePlatformBootstrap />
           <AuthProvider>
             <AuthDiagnostics />
